ae2b45a37d32fcb8120d875d7f65611654cadb9e
Louise Lynne Seprado <121284201+louith@users.noreply.github.com>
2025-03-30 12:09:21 +0800

    feat: pokedex app (midterm exam)

--- server.js
+++ server.js
@@ -1,33 +1,64 @@
-const { name } = require("ejs");
 const express = require("express");
 const axios = require("axios");
+const { name } = require("ejs");
 const app = express();
-app.set("view engine", "ejs");
-app.set("views", "./views");
-app.use(express.static("public"));
+const port = 3000;
 
-const port = 4000;
+//set EJS as the templating engine
+app.set("view engine", "ejs");
+// app.set("views", "./views");
+app.use(express.static("public"));
 
 app.get("/", async (req, res) => {
   try {
     const response = await axios.get(
-      "https://jsonplaceholder.typicode.com/posts"
+      "https://pokeapi.co/api/v2/pokemon?limit=30"
     );
-    res.render("index", { posts: response.data });
+    const pokemonList = await Promise.all(
+      response.data.results.map(async (pokemon) => {
+        const details = await axios.get(pokemon.url);
+        return {
+          name: pokemon.name,
+          image: details.data.sprites.front_default,
+          url: `/pokemon/${details.data.id}`,
+        };
+      })
+    );
+    res.render("index", { pokemonList });
   } catch (error) {
-    res.status(500).send("Error fetching data");
+    res.status(500).send("Error fetching Pokemon data");
   }
 });
 
-app.get("/:id", async (req, res) => {
+app.get("/pokemon/:id", async (req, res) => {
   try {
-    const postId = req.params.id;
-    const response = await axios.get(
-      `https://jsonplaceholder.typicode.com/posts/${postId}`
+    const { id } = req.params;
+    const response = await axios.get(`https://pokeapi.co/api/v2/pokemon/${id}`);
+    const speciesResponse = await axios.get(
+      `https://pokeapi.co/api/v2/pokemon-species/${id}`
     );
-    res.render("post", { post: response.data });
+
+    const pokemon = {
+      name: response.data.name,
+      image: response.data.sprites.other.dream_world.front_default,
+      weight: response.data.weight,
+      height: response.data.height,
+      moves: response.data.moves.slice(0, 5).map((m) => m.move.name),
+      description: speciesResponse.data.flavor_text_entries.find(
+        (entry) => entry.language.name === "en"
+      ).flavor_text,
+      stats: {
+        hp: response.data.stats[0].base_stat,
+        attack: response.data.stats[1].base_stat,
+        defense: response.data.stats[2].base_stat,
+        special_attack: response.data.stats[3].base_stat,
+        special_defense: response.data.stats[4].base_stat,
+        speed: response.data.stats[5].base_stat,
+      },
+    };
+    res.render("pokemon", { pokemon });
   } catch (error) {
-    res.status(500).send("Error fetching post details");
+    res.status(500).send("Error fetching Pokémon details");
   }
 });
 
