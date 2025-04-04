cc1500795b4500d810a4b248d296ffbbbcdf825d
Louise Lynne Seprado <121284201+louith@users.noreply.github.com>
2025-04-05 00:14:34 +0800

    feat: building api with crud operations

--- server.js
+++ server.js
@@ -6,64 +6,63 @@ const port = 3000;
 
 //set EJS as the templating engine
 app.set("view engine", "ejs");
-// app.set("views", "./views");
 app.use(express.static("public"));
+app.use(express.json()); // Middleware to parse JSON bodies
 
-app.get("/", async (req, res) => {
-  try {
-    const response = await axios.get(
-      "https://pokeapi.co/api/v2/pokemon?limit=30"
-    );
-    const pokemonList = await Promise.all(
-      response.data.results.map(async (pokemon) => {
-        const details = await axios.get(pokemon.url);
-        return {
-          name: pokemon.name,
-          image: details.data.sprites.front_default,
-          url: `/pokemon/${details.data.id}`,
-        };
-      })
-    );
-    res.render("index", { pokemonList });
-  } catch (error) {
-    res.status(500).send("Error fetching Pokemon data");
-  }
-});
-
-app.get("/pokemon/:id", async (req, res) => {
-  try {
-    const { id } = req.params;
-    const response = await axios.get(`https://pokeapi.co/api/v2/pokemon/${id}`);
-    const speciesResponse = await axios.get(
-      `https://pokeapi.co/api/v2/pokemon-species/${id}`
-    );
-
-    const pokemon = {
-      name: response.data.name,
-      image: response.data.sprites.other.dream_world.front_default,
-      weight: response.data.weight,
-      height: response.data.height,
-      moves: response.data.moves.slice(0, 5).map((m) => m.move.name),
-      description: speciesResponse.data.flavor_text_entries.find(
-        (entry) => entry.language.name === "en"
-      ).flavor_text,
-      stats: {
-        hp: response.data.stats[0].base_stat,
-        attack: response.data.stats[1].base_stat,
-        defense: response.data.stats[2].base_stat,
-        special_attack: response.data.stats[3].base_stat,
-        special_defense: response.data.stats[4].base_stat,
-        speed: response.data.stats[5].base_stat,
-      },
-    };
-    res.render("pokemon", { pokemon });
-  } catch (error) {
-    res.status(500).send("Error fetching Pokémon details");
-  }
-});
+const users = [
+  { id: 1, name: "John Doe" },
+  { id: 2, name: "Anne Klutz" },
+  { id: 3, name: "Hi" },
+];
 
 app.get("/", (req, res) => {
-  res.send("Hello World");
+  res.send("Homepage");
+});
+
+app.get("/users", (req, res) => {
+  res.json(users);
+});
+
+app.get("/users/:id", (req, res) => {
+  const user = users.find((u) => u.id === parseInt(req.params.id));
+  if (user) {
+    res.json(user);
+  } else {
+    res.status(404).send("User not found");
+  }
+});
+
+//create new user
+app.post("/users", (req, res) => {
+  const newUser = {
+    id: Date.now(),
+    name: req.body.name,
+  };
+  // Normally, you would save this user to a database
+  users.push(newUser);
+  // For this example, we'll just push it to the array
+  res.status(201).json(newUser);
+});
+
+//update
+app.put("/users/:id", (req, res) => {
+  const user = users.find((u) => u.id === parseInt(req.params.id));
+  if (user) {
+    user.name = req.body.name;
+    res.json(user);
+  } else {
+    res.status(404).send("User not found");
+  }
+});
+
+app.delete("/users/:id", (req, res) => {
+  const userIndex = users.findIndex((u) => u.id === parseInt(req.params.id));
+  if (userIndex !== -1) {
+    users.splice(userIndex, 1); // Remove the user from the array
+    res.send("User deleted");
+  } else {
+    res.status(404).send("User not found");
+  }
 });
 
 // const postRouter = require("./routes/posts");
