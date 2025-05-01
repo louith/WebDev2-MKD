3045d69444c4b2a8f507dcb32fbff41038c416aa
Louise Lynne Seprado <121284201+louith@users.noreply.github.com>
2025-05-02 00:42:46 +0800

    with form activity changes

--- db.js
+++ db.js
@@ -1,11 +1,11 @@
 // db.js
 const Database = require("better-sqlite3");
-const db = new Database("users.db");
+const db = new Database("applicants.db");
 
 // Create the users table if it doesn't exist
 db.prepare(
   `
-  CREATE TABLE IF NOT EXISTS users (
+  CREATE TABLE IF NOT EXISTS applicants (
     id INTEGER PRIMARY KEY AUTOINCREMENT,
     name TEXT NOT NULL,
     email TEXT NOT NULL
